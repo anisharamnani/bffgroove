c84e75e4d1183c17865a04f77ccd3a7e38971ba4
sranso <sranso@gmail.com>
2014-03-10 17:23:51 -0400

    remove to_s b/c not necessary

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -127,8 +127,8 @@ class Campaign < ActiveRecord::Base
   end
 
   def self.date_range(from,to)
-    datecampaignsfrom = from.to_s + "T00:00:00Z"
-    datecampaignsto = to.to_s + "T23:59:59Z"
+    datecampaignsfrom = from + "T00:00:00Z"
+    datecampaignsto = to + "T23:59:59Z"
     campaigns = Campaign.where(:send_date => datecampaignsfrom..datecampaignsto)
   end
 
