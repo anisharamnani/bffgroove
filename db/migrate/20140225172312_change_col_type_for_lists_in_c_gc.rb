9ab03579f21407db944aac80358f7bb084101a18
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-03-11 15:27:27 -0400

    edit migration again

--- db/migrate/20140225172312_change_col_type_for_lists_in_c_gc.rb
+++ db/migrate/20140225172312_change_col_type_for_lists_in_c_gc.rb
@@ -1,11 +1,11 @@
 class ChangeColTypeForListsInCGc < ActiveRecord::Migration
   def up
     execute <<-SQL
+      sp_RENAME 'campaigns.list', 'list_id', 'COLUMN';
       ALTER TABLE campaigns
-      RENAME COLUMN list to list_id
       ALTER COLUMN list_id TYPE int;
+      sp_RENAME 'group_campaigns.list', 'list_id', 'COLUMN';
       ALTER TABLE group_campaigns
-      RENAME COLUMN list to list_id
       ALTER COLUMN list_id TYPE int
     SQL
     # rename_column :campaigns, :list, :list_id
