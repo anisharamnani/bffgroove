c4c182988389f24fd2680b166337ce307d9f0ba3
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-03-11 15:37:37 -0400

    last try, aliens

--- db/migrate/20140225172312_change_col_type_for_lists_in_c_gc.rb
+++ db/migrate/20140225172312_change_col_type_for_lists_in_c_gc.rb
@@ -1,23 +1,15 @@
 class ChangeColTypeForListsInCGc < ActiveRecord::Migration
   def up
-    execute <<-SQL
-      EXEC sp_RENAME 'campaigns.list', 'list_id', 'COLUMN';
-      ALTER TABLE campaigns
-      ALTER COLUMN list_id TYPE int;
-      EXEC sp_RENAME 'group_campaigns.list', 'list_id', 'COLUMN';
-      ALTER TABLE group_campaigns
-      ALTER COLUMN list_id TYPE int
-    SQL
-    # rename_column :campaigns, :list, :list_id
-    # change_column :campaigns, :list_id, :integer
-    # rename_column :group_campaigns, :list, :list_id
-    # change_column :group_campaigns, :list_id, :integer
+    remove_column :campaigns, :list
+    add_column :campaigns, :list_id, :integer
+    remove_column :group_campaigns, :list
+    add_column :group_campaigns, :list_id, :integer
   end
 
   def down
-    rename_column :campaigns, :list_id, :list
-    change_column :campaigns, :list, :string
-    rename_column :group_campaigns, :list_id, :list
-    change_column :group_campaigns, :list, :string
+    remove_column :campaigns, :list_id
+    add_column :campaigns, :list, :string
+    remove_column :group_campaigns, :list_id
+    add_column :group_campaigns, :list, :string
   end
 end
