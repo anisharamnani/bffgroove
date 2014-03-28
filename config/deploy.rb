70c745ab3be8dca172f7bb8cdbe8d3d11e029b75
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-03-28 13:28:44 -0400

    change ssh key

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :branch, "master"
 
 
 default_run_options[:pty] = true
-ssh_options[:keys] = %w(~/.ssh/github_rsa)
+ssh_options[:keys] = %w(~/.ssh/id_rsa.pub)
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
